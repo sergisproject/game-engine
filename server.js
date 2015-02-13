65840405c4e712e12f630effc310fce3414557f4
Jake Hartz <jhartz@outlook.com>
2015-02-13 13:12:29 -0500

    Added structural files

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 /*
-    The SerGIS Project - sergis-game
-
     Copyright (c) 2015, SerGIS Project Contributors. All rights reserved.
     Use of this source code is governed by the MIT License, which can be found
     in the LICENSE.txt file.
- */
+*/
 
 // node modules
 var fs = require("fs"),
