953ba3aec8af392bd2a8c5d4b8629b774aeecba8
Jake Hartz <jhartz@outlook.com>
2015-02-15 19:58:56 -0500

    Started adding code to the programming project...

--- modules/pageServers/contentServer.js
+++ modules/pageServers/contentServer.js
@@ -12,11 +12,46 @@ var fs = require("fs"),
 var router = require("express").Router();
 
 // our modules
-var config = require("../../config");
+var config = require("../../config"),
+    writer = require("../../modules/writer");
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+var TEST_CONTROLS = {
+    /*
+    id: {
+        type: "type here",
+        data: {
+            // data here
+        }
+    }
+    */
+    1: {
+        type: "basic",
+        data: {}
+    }
+};
+////////////////////////////////////////////////////////////////////////////////
+
+
 
 // Handler for GET requests to /content/CONTENT ID HERE
 router.get("/:id", function (req, res) {
-    res.end("Content handler for id " + req.params.id);
+    var id = Number(req.params.id);
+    if (isNaN(id) || !TEST_CONTROLS.hasOwnProperty(id)) {
+        // "Bad Request"
+        writer.writeError(res, 400);
+    } else {
+        writer.write(res, "control.html", {
+            "socket-io-src": config.SOCKET_IO_ORIGIN + "/socket.io/socket.io.js",
+            
+            // NOTE: The following are written to JS variables!
+            "socket-io-location": config.SOCKET_IO_ORIGIN + "/content",
+            "content-type": JSON.stringify(TEST_CONTROLS[id].type),
+            "content-data": JSON.stringify(TEST_CONTROLS[id].data)
+        });
+    }
 });
 
 // Export the router
