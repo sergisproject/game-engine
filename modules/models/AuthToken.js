c7f8ddfd00a57af6ada7d56d6b86b652122394a0
Jake Hartz <jhartz@outlook.com>
2015-02-24 11:48:27 -0500

    Added auth token generator

--- modules/models/AuthToken.js
+++ modules/models/AuthToken.js
@@ -4,8 +4,31 @@
     in the LICENSE.txt file.
 */
 
+// node modules
+var crypto = require("crypto");
+
 // our modules
 var config = require("../../config");
+
+
+// The length of the random parts of our auth tokens
+var TOKEN_LENGTH = 14;
+
+
+/**
+ * Generate a random, pretty guaranteed unique auth token.
+ */
+function generateToken() {
+    return crypto.randomBytes(TOKEN_LENGTH)
+        .toString("base64")
+        .substring(0, TOKEN_LENGTH)
+        // Turn obscure base64 characters into less obscure characters
+        .replace(/+/g, ".")
+        .replace(/\//g, ",")
+        // Add the current time for more uniqueness
+        + (new Date()).getTime().toString(36);
+}
+
 
 module.exports = function (mongoose) {
     var Schema = mongoose.Schema;
@@ -27,6 +50,10 @@ module.exports = function (mongoose) {
             default: Date.now
         }
     });
+    
+    authTokenSchema.methods.generateToken = function () {
+        return (this.token = generateToken());
+    };
     
     return mongoose.model("AuthToken", authTokenSchema);
 };
